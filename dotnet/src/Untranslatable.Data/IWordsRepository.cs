df219f23517abe43447aa9c6da61d5d07c761387
skrimokst <skrimokst@users.noreply.github.com>
2022-08-15 11:07:33 +0100

    Update IWordsRepository.cs

--- dotnet/src/Untranslatable.Data/IWordsRepository.cs
+++ dotnet/src/Untranslatable.Data/IWordsRepository.cs
@@ -5,7 +5,7 @@ namespace Untranslatable.Data
 {
     public interface IWordsRepository
     {
-        public IEnumerable<UntranslatableWord> GetByLanguage(string language, CancellationToken cancellationToken = default);
+        IEnumerable<UntranslatableWord> GetByLanguage(string language, CancellationToken cancellationToken = default);
         UntranslatableWord GetRandom(CancellationToken cancellationToken = default);
     }
 }
