60c3e66b84c6a9b91bdd09034ec6c958ed6f1c66
Joana Carvalho <joana.carvalho@sage.com>
2026-05-24 18:55:35 +0100

    refactor: simplify ToDto method in UntranslatableWordExtensions for improved readability and null handling

--- dotnet/src/Untranslatable.Api/Controllers/Extensions/UntranslatableWordExtensions.cs
+++ dotnet/src/Untranslatable.Api/Controllers/Extensions/UntranslatableWordExtensions.cs
@@ -1,21 +1,18 @@
-﻿using Untranslatable.Api.Models;
+using Untranslatable.Api.Models;
 using Untranslatable.Data;
 
 namespace Untranslatable.Api.Controllers.Extensions
 {
     internal static class UntranslatableWordExtensions
     {
-        public static UntranslatableWordDto ToDto(this UntranslatableWord untranslatableWord)
-        {
-            if (untranslatableWord is null)
-                return null;
-
-            return new UntranslatableWordDto
-            {
-                Language = untranslatableWord.Language,
-                Meaning = untranslatableWord.Meaning,
-                Word = untranslatableWord.Word
-            };
-        }
+        public static UntranslatableWordDto? ToDto(this UntranslatableWord? word) =>
+            word is null
+                ? null
+                : new UntranslatableWordDto
+                {
+                    Language = word.Language,
+                    Word = word.Word,
+                    Meaning = word.Meaning,
+                };
     }
 }
